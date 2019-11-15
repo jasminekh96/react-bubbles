3c299bfc5b6337b2e231c8769d62bc55dd505b1c
Jasmine Kasheboon Khoury <jasminekh96@gmail.com>
2019-11-15 13:05:08 -0500

    delete and updateworks/MVP

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -9,7 +9,12 @@ const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
-
+  useEffect(()=>
+  {
+    axiosWithAuth().get("/api/colors")
+    .then((res) => setColorList(res.data))
+    .catch((err) => console.log(err.res));
+  })
 
   return (
     <>
